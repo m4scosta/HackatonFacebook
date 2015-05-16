acaa3df1d69b7fe805d8a08d83c17b4701c96cfe
Erich Rodrigues Ferrares <erich.rodriguesf@gmail.com>
2015-05-16 14:27:47 -0300

    Adiciona modelos da app Recipe

--- HackatonFacebook/recipe/models.py
+++ HackatonFacebook/recipe/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Unit(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    user_add = models.ForeignKey(User)
+    date_add = models.DateField(auto_now_add=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    preparation = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe)
+    ingredient = models.ForeignKey(Ingredient)
+    quantity = models.DecimalField(decimal_places=4, max_digits=10)
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User)
+    recipe = models.ForeignKey(Recipe)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User)
+    recipe = models.ForeignKey(Recipe)
