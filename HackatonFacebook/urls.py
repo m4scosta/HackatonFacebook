fcc848075a1258076acb94249193dd4e86606ad0
Erich Rodrigues Ferrares <erich.rodriguesf@gmail.com>
2015-05-16 15:55:57 -0300

    Adiciona urls base correta..

--- HackatonFacebook/urls.py
+++ HackatonFacebook/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns(
     '',
     url(r'^get/teste', '%s.usecase.ajax.get_by_ajax' % _BASE_AJAX, name='teste'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('HackatonFacebook.recipe.urls')),
+    url(r'^', include('HackatonFacebook.recipe.urls')),
     url(r'^ajax/', '%s.ajax.ajax.home' % _BASE_VIEW)
 )
