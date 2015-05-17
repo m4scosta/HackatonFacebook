3fe50bb7676ac09c1240d71944cec0ab640901d7
bustamante <marcos.bustamante@fatec.sp.gov.br>
2015-05-17 04:19:20 -0300

    Função de ordenar

--- HackatonFacebook/static/components/listProducts/listProducts.js
+++ HackatonFacebook/static/components/listProducts/listProducts.js
@@ -15,8 +15,11 @@ angular.module('HackathonApp').directive("listProducts", function(){
             $scope.openProduct = function(index){
                 $scope.openproduct = $scope.recipes[index];
                 $scope.viewproduct = true;
+            };
+
+            $scope.myOrder = function(recipe, recipe2){
+                return recipe.count_have - recipe.count_no_have
             }
         }
     }
 });
-
