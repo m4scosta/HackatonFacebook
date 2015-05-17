d8ffa79170a96ed5a9765e3473fa467b9301c00d
Marcos Costa <marcos_costa.sjc@hotmail.com>
2015-05-17 04:59:32 -0300

    Ajustados as acoes no facebook

--- HackatonFacebook/static/components/navbar/navbar.js
+++ HackatonFacebook/static/components/navbar/navbar.js
@@ -6,7 +6,22 @@ angular.module('HackathonApp').directive('navbarHack', function(){
     return {
         restrict: 'E',
         templateUrl: 'static/components/navbar/navbar.html',
-        link: function(element, attr, scope){
+        controller: function($scope, $window){
+
+        	$scope.login = function() {
+        		if ($window.FB !== undefined) {
+        			$window.FB.getLoginStatus(function (response) {
+        				$window.FB.getLoginStatus(function (response) {
+	                        if (response.status !== "connected")  {
+	                            FB.login(function(response) {}, {
+	                               scope: 'publish_actions', 
+	                               return_scopes: true
+	                            });
+	                        }
+	                    });
+        			});
+        		}
+        	}
         }
     }
 });
