f82f33cecfc646c461880f802a3d00ec68d6b099
Marcos Costa <marcos_costa.sjc@hotmail.com>
2015-05-17 04:59:32 -0300

    Ajustados as acoes no facebook

--- HackatonFacebook/static/components/openProduct/openProduct.js
+++ HackatonFacebook/static/components/openProduct/openProduct.js
@@ -11,7 +11,7 @@ angular.module('HackathonApp').directive("openProduct", function(){
             openproduct: '='
         },
         templateUrl: 'static/components/openProduct/openProduct.html',
-        controller: function($scope, Ajax) {
+        controller: function($scope, Ajax, $window) {
             $scope.closeProduct = function(){
                 $scope.viewproduct = false;
             };
@@ -30,6 +30,36 @@ angular.module('HackathonApp').directive("openProduct", function(){
                 }).error(function(){
 
                 });
+            };
+
+            $scope.publishRecipe = function() {
+
+                if ($window.FB != undefined) {
+
+                    $window.FB.getLoginStatus(function (response) {
+                        if (response.status === "connected") {
+
+                            body = "Ola pessoal quero fazer a receita " + $scope.openproduct.name + ".\n" +
+                                "Mas me faltam alguns ingredientes, alguem gostaria de " +
+                                "se juntar a mim e trazer o que falta?\n\n" + $scope.openproduct.ingredients_out.join("\n");
+
+                            $window.FB.api("/me/feed", "post", {message: body}, function (response) {
+                                if (!response || response.error) {
+                                    alert("Nao foi possivel postar, tente novamente.");
+                                } else {
+                                    alert("Postado com sucesso na sua timeline");
+                                }
+                            });
+
+                        } else {
+                            FB.login(function(response) {}, {
+                               scope: 'publish_actions', 
+                               return_scopes: true
+                            });
+                        }
+                    });
+
+                }
             };
         }
     }
